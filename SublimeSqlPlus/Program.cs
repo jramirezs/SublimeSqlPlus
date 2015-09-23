bf1211520863442775f15cd4e8a25181e88879d2
Jorge Ramirez <jramirez@isf.com.co>
2015-09-23 17:28:13 -0500

    Refactor Cmd execution

--- SublimeSqlPlus/Program.cs
+++ SublimeSqlPlus/Program.cs
@@ -26,51 +26,80 @@ namespace SublimeSqlWrapper {
         }
     }
 
+    public class CmdWrapper {
+        private Process _process;
+        private IList<string> _commands { get; set; }
+
+        public string Program { get; set; }
+        private bool HasStarted { get; set; }
+
+        public CmdWrapper(string program) {
+            this.Program = program;
+            this.Restart();
+        }
+
+        public void Restart() {
+            _process = new Process();
+            _process.StartInfo.FileName = Program; //Program must be on the machine path
+            _process.StartInfo.UseShellExecute = false;
+            _process.StartInfo.RedirectStandardInput = true;
+            _process.StartInfo.RedirectStandardOutput = true;
+
+            _commands = new List<string>();
+        }
+
+        public void AddCommand(string command) {
+            _commands.Add(command);
+        }
+
+        public string ExecuteCommands() {
+            if (!this.HasStarted) {
+                _process.Start();
+                this.HasStarted = true;
+            }
+
+            using (StreamWriter sw = _process.StandardInput) {
+                if (sw.BaseStream.CanWrite) {
+                    foreach (var command in _commands) {
+                        sw.WriteLine(command);   
+                    }
+                }
+            }
+
+            _process.WaitForExit();
+
+            return _process.StandardOutput.ReadToEnd();
+        }
+    }
+
     public class Program {
         static void Main(string[] args) {
-            Process process = new Process();
-            process.StartInfo.FileName = "sqlplus"; //SqlPlus must be on the machine path
-            process.StartInfo.UseShellExecute = false;
-            process.StartInfo.RedirectStandardInput = true;
-            process.StartInfo.RedirectStandardOutput = true;
+            CmdWrapper cmd = new CmdWrapper("sqlplus");
 
             var input = new CommandArguments(args);
             var output = string.Empty;
             var fileName = CreateTempFile(input.FilePath);
 
-            process.Start();
-            using (StreamWriter sw = process.StandardInput) {
-                if (sw.BaseStream.CanWrite) {
-                    //Connect to Database
-                    sw.WriteLine("{0}/{1}@{2}", input.DatabaseName, input.DatabasePassword, input.DatabaseServer);
-
-                    //Execute File
-                    sw.WriteLine("@\"{0}\"", fileName);
-                }
-            }
+            cmd.AddCommand(string.Format("{0}/{1}@{2}", input.DatabaseName, input.DatabasePassword, input.DatabaseServer));
+            cmd.AddCommand(string.Format("@\"{0}\"", fileName));
 
             //Store output results
-            output = process.StandardOutput.ReadToEnd();
-            process.WaitForExit();
-
+            output = cmd.ExecuteCommands();
+            
             File.Delete(fileName);
 
             //Check for errors and run errors query
             if (output.ToLower().Contains("compilation errors")) {
-                process.Start();
-                using (StreamWriter sw = process.StandardInput) {
-                    if (sw.BaseStream.CanWrite) {
-                        //Connect to Database
-                        sw.WriteLine("{0}/{1}@{2}", input.DatabaseName, input.DatabasePassword, input.DatabaseServer);
+                cmd.Restart();
 
-                        //Show Errors
-                        sw.Write(File.ReadAllText("ErrorsQuery.sql"), input.DatabaseName, Path.GetFileNameWithoutExtension(input.FilePath));
-                    }
-                }
+                //Connect to Database
+                cmd.AddCommand(string.Format("{0}/{1}@{2}", input.DatabaseName, input.DatabasePassword, input.DatabaseServer));
+
+                //Show Errors
+                cmd.AddCommand(string.Format(File.ReadAllText("ErrorsQuery.sql"), input.DatabaseName, Path.GetFileNameWithoutExtension(input.FilePath)));
 
                 //Store output errors
-                output = process.StandardOutput.ReadToEnd();
-                process.WaitForExit();
+                output = cmd.ExecuteCommands();
             }
 
             Console.Write(output);
@@ -86,8 +115,8 @@ namespace SublimeSqlWrapper {
 
             //Add SqlPlus finish token (/)
             var fileContent = File.AppendText(tempFileName);
-            
-            fileContent.WriteLine(); 
+
+            fileContent.WriteLine();
             fileContent.WriteLine("/");
 
             fileContent.Close();
